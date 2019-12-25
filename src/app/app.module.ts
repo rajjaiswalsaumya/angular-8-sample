56b655fc4a4952c7733100258b3b966900c41724
rajjaiswalsaumya <rajjaiswalsaumya@gmail.com>
2019-12-25 17:50:33 +0530

    Partial Store Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { RouterModule } from "@angular/router";
-import { ReactiveFormsModule } from "@angular/forms";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from "./app.routing.module";
+import { AppRoutingModule } from './app.routing.module';
 
-import { AppComponent } from "./app.component";
+
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,8 +16,7 @@ import { AppComponent } from "./app.component";
     BrowserModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ]
 })
 export class AppModule {}
